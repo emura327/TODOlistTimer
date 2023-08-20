a57e5fd65d6dce4d7339f257fff7316bdd34f460
EmuraRyota <emuryo.150327@ezweb.ne.jp>
2023-08-20 15:59:17 +0900

    いらないものの削除

--- Main.java
+++ Main.java
@@ -1,22 +1,13 @@
-/**
- * グラフィカルな時計を表示するアプリケーション。
- * 
- * @author M.Nogi
- */
-public class Main extends Object {
+import java.io.IOException;
+
+public class Main {
     
-    /**
-     * エントリポイント。
-     * グラフィカルな時計を生成して実行する。
-     * 
-     * @param args 実行時引数。特に使用しない。
-     */
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+        throws IOException {
         
-        WidgetClock aClock = new WidgetClock();
-
-        aClock.perform();
-
+        TodoList list = new TodoList();
+        list.perform();
+        
     }
 
 }
